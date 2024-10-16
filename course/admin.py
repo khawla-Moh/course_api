a631d671d37af44816cf7ad3e65959ab691fba2c
khawla-Moh <khawlaMoh00@gmail.com>
2024-10-17 00:04:27 +0300

    add courses and category models

--- course/admin.py
+++ course/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Courses,Category
 
 # Register your models here.
+
+admin.register(Courses)
+admin.register(Category)
--- course/models.py
+++ course/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Course(models.Model):
+class Courses(models.Model):
     name=models.CharField(max_length=255)
     description=models.TextField()
     price=models.DecimalField(max_digits=10,decimal_places=2)
     category=models.ForeignKey('Category',related_name='course_category',on_delete=models.SET_NULL,null=True)
     image=models.ImageField(upload_to='post')
-    slug=models.SlugField(blank=True,null=True , unique=True)
     
 
     def __str__(self):
